70ff3d8e0792165610dfdd188a7b23958a21dd15
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-11-08 21:24:47 -0500

    move test contracts

--- src/test/StakingRewards.t.sol
+++ src/test/StakingRewards.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.6;
 
 import "ds-test/test.sol";
 
-import "./StakingRewards.sol";
+import "../StakingRewards.sol";
 
 contract StakingRewardsTest is DSTest {
     StakingRewards rewards;
